ed3f68b827178e90a0028f6ed3d7cac4bbaa3532
JhonierRojas <yonier202@gmail.com>
2023-08-28 11:10:46 -0500

    avance busqueda con sql

--- php/usuario_lista.php
+++ php/usuario_lista.php
@@ -1,3 +1,22 @@
 <?php
 
+// desde donde vamos a mostrar los registros segun al pagina en que estemos
+$inicio=($pagina>0) ? (($registros*$pagina)-$registros) : 0;
+$tabla="";
+
+if (isset($busqueda) && $busqueda!=""){
+    $consulta_datos="SELECT * FROM usuario WHERE ((`id`!= '".$_SESSION['id']."')
+    AND (`nombre` LIKE "%$busqueda%" OR `apellido` LIKE "%$busqueda%" OR `usuario` LIKE "%$busqueda%"
+    OR `email` LIKE "%$busqueda%" )) order by nombre ASC limit $inicio,$registros";
+
+    $consulta_total="SELECT COUNT(id) FROM usuario WHERE (`id`!= '".$_SESSION['id']."')
+    AND (`nombre` LIKE "%$busqueda%" OR `apellido` LIKE "%$busqueda%" OR `usuario` LIKE "%$busqueda%"
+    OR `email` LIKE "%$busqueda%" ))";
+
+}else{
+    $consulta_datos="SELECT * FROM usuario WHERE `id`!= '".$_SESSION['id']."' order by nombre ASC limit $inicio,$registros";
+
+    $consulta_total="SELECT COUNT(id) FROM usuario WHERE id=! '".$_SESSION['id']."'";
+}
+$conexion=conexion();
 ?>
