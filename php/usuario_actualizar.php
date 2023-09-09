223151535a3e587edd537a07b3024b02270897e2
JhonierRojas <yonier202@gmail.com>
2023-09-09 10:34:51 -0500

    verificar datos actualizar

--- php/usuario_actualizar.php
+++ php/usuario_actualizar.php
@@ -20,5 +20,113 @@
         
     }
     $check_usuario=null;
+
+    $admin_usuario=limpiar_cadena($_REQUEST['administrador_usuario']);
+    $admin_clave=limpiar_cadena($_REQUEST['administrador_clave']);
+
+    //verificar que los campos no esten vacios
+
+    if ($admin_usuario=="" || $admin_clave=="") {
+    echo '
+    <div class="notification is-danger is-light">
+        <strong>¡Ocurrio un error inesperado!</strong><br>
+        No has llenado todos los campos que son obligatorios
+    </div>';
+    exit();
+    }
+
+    //VERIFICAR EL TIPO DE DATO CORRESPONDIENTE
+    
+    if (verificar_datos("[a-zA-Z0-9]{4,20}",$admin_usuario)) {
+        echo '
+        <div class="notification is-danger is-light">
+            <strong>¡Ocurrio un error inesperado!</strong><br>
+            El Usuario no coincide con el formato solicitado
+        </div>';
+        exit();
+    }
+
+    if (verificar_datos("[a-zA-Z0-9$@.-]{7,100}",$admin_clave)) {
+        echo '
+        <div class="notification is-danger is-light">
+            <strong>¡Ocurrio un error inesperado!</strong><br>
+            La Clave no coincide con el formato solicitado
+        </div>';
+        exit();
+    }
+
+    //verificando admin
+    $check_admin=conexion();
+
+    $check_admin=$check_admin->query("SELECT usuario, clave FROM usuario WHERE usuario='$admin_usuario' AND id=".$_SESSION['id']);
+    if ($check_admin->rowCount()==1) {
+        //contiene usuario o clave
+        $check_admin=$check_admin->fetch();
+        //si no se cumple usuario y clave correctos
+        if ($check_admin['usuario']!=$admin_usuario || !password_verify($admin_clave, $check_admin['clave'])) {
+            echo '
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                USUARIO o CLAVE Administrador incorrectos
+            </div>';
+            exit();
+        }
+
+    }else{
+        echo '
+        <div class="notification is-danger is-light">
+            <strong>¡Ocurrio un error inesperado!</strong><br>
+            USUARIO o CLAVE Administrador incorrectos
+        </div>';
+        exit();
+    }
+    $check_admin=null;
+    
+    $nombre=limpiar_cadena($_REQUEST['usuario_nombre']);
+    $apellido=limpiar_cadena($_REQUEST['usuario_apellido']);
+    $usuario=limpiar_cadena($_REQUEST['usuario_usuario']);
+    $email=limpiar_cadena($_REQUEST['usuario_email']);
+    $clave1=limpiar_cadena($_REQUEST['usuario_clave_1']);
+    $clave2=limpiar_cadena($_REQUEST['usuario_clave_2']);
+
+    if ($nombre=="" || $apellido=="" || $usuario=="") {
+        echo '
+        <div class="notification is-danger is-light">
+            <strong>¡Ocurrio un error inesperado!</strong><br>
+            No has llenado todos los campos que son obligatorios
+        </div>';
+        exit();
+    }
+    
+    //verificando los tipos de datos en los inputs
+
+    if (verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}",$nombre)) {
+        echo '
+        <div class="notification is-danger is-light">
+            <strong>¡Ocurrio un error inesperado!</strong><br>
+            El Nombre no coincide con el formato solicitado
+        </div>';
+        exit();
+    }
+
+    if (verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}",$apellido)) {
+        echo '
+        <div class="notification is-danger is-light">
+            <strong>¡Ocurrio un error inesperado!</strong><br>
+            El Apellido no coincide con el formato solicitado
+        </div>';
+        exit();
+    }
+
+
+    if (verificar_datos("[a-zA-Z0-9]{4,20}",$usuario)) {
+        echo '
+        <div class="notification is-danger is-light">
+            <strong>¡Ocurrio un error inesperado!</strong><br>
+            El Usuario no coincide con el formato solicitado
+        </div>';
+        exit();
+    }
+
  
 ?>
