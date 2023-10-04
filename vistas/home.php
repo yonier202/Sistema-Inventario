cf86510fb71468112cb416ce56126d990bd2de5d
JhonierRojas <yonier202@gmail.com>
2023-10-04 11:32:10 -0500

    cambio diseño home

--- vistas/home.php
+++ vistas/home.php
@@ -1,19 +1,13 @@
-<div class="container is-fluid">
-    <h1 class="title">Home</h1>
-    <h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['name']." ".$_SESSION['apellido']; ?>! </h2>
-</div>
+<section class="hero is-fullheight is-primary">
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <h1 class="title is-1">
+                Bienvenido a Sistmema Inventario
+            </h1>
+            <h2 class="subtitle is-3">
+                ¡Hola, <?php echo $_SESSION['name']." ".$_SESSION['apellido']; ?>!
+            </h2>
+        </div>
+    </div>
 
-<!-- <nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">
-  <a class="pagination-previous" href="#">Anterior</a>
-  
-  <ul class="pagination-list">
-    <li><a class="pagination-link" href="#">1</a></li>
-    <li><span class="pagination-ellipsis">&hellip;</span></li>
-    <li><a class="pagination-link is-current" href="#">2</a></li>
-    <li><a class="pagination-link" href="#">3</a></li>
-    <li><span class="pagination-ellipsis">&hellip;</span></li>
-    <li><a class="pagination-link" href="#"">3</a></li>
-  </ul>
-
-  <a class="pagination-next" href="#">Siguiente</a>
-</nav> -->
+</section>
